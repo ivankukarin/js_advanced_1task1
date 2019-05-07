41b5a397861144066fd5e7a2c90339fd7d52382c
ivankukarin <ikukarin@yandex.ru>
2019-05-07 17:12:22 +0400

    not good

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
 let arrayPrices = [200, 550, 4000, 23, 58, 5000, 485, 711];
+let minGamePoints = 10000;
 let arraySum = (array) =>
 	{
 		let sum = 0;
 		for (let value of array) 
-			sum += value;
+			{sum += value};
 		console.log (sum);
-	}
+	};
 
-const bonus = (array) => { Math.round (
-	for (let value of prices) {
-		if (value >=)
-		console.log(value * 2);
-		}
-	);
 
-};
+
+const bonus = (array) => 
+	if (arraySum(array) > 10000 ) {
+		return (arraySum(array) * 5%);
+	};
+
+console.log(bonus(arrayPrices));
 

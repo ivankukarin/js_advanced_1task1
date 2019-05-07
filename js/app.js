ab3bd8e0d60d95025aebca662c44330d33041e83
ivankukarin <ikukarin@yandex.ru>
2019-05-07 12:54:05 +0400

    no comm

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let arrayPrices = [200, 550, 4000, 23, 58, 5000, 485, 711];
+let arraySum = (array) =>
+	{
+		let sum = 0;
+		for (let value of array) 
+			sum += value;
+		console.log (sum);
+	}
+
+const bonus = (array) => { Math.round (
+	for (let value of prices) {
+		if (value >=)
+		console.log(value * 2);
+		}
+	);
+
+	};
+

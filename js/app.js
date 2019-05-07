454794c2a61233634a38c8a0e4a35c9aab2504f4
ivankukarin <ikukarin@yandex.ru>
2019-05-07 19:27:42 +0400

    считает сумму, но не считает бонус ставит склонение но не ставит в строку из-за бонуса

--- js/app.js
+++ js/app.js
@@ -10,10 +10,27 @@ let arraySum = (array) =>
 
 
 
-const bonus = (array) => 
+const bonus = (array) => {
 	if (arraySum(array) > 10000 ) {
-		return (arraySum(array) * 5%);
+		arraySum(array) * 0.05;
 	};
+};
 
 console.log(bonus(arrayPrices));
+
+// это для теста второй задачи
+// let a = prompt ('Сколько баллов');
+
+let num2str = function(x) {  
+        let n = Math.abs(x) % 100; 
+        let n1 = n % 10;
+        let textForms = ["балл", "балла", "баллов"];
+        if (n > 10 && n < 20) { return textForms[2]; }
+        if (n1 > 1 && n1 < 5) { return textForms[1]; }
+        if (n1 == 1) { return textForms[0]; }
+        return textForms[2];
+    }
+
+
+console.log (bonus(arrayPrices) + num2str(bonus(arrayPrices)));
 

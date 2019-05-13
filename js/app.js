2db2637ae00175d9893bc4859fb6ee7f479f5d35
ivankukarin <ikukarin@yandex.ru>
2019-05-13 17:26:22 +0400

    СДелал задачу 1 и 2

--- js/app.js
+++ js/app.js
@@ -1,37 +1,48 @@
+'use strict';
+
 let arrayPrices = [200, 550, 4000, 23, 58, 5000, 485, 711];
 let minGamePoints = 10000;
-const arraySum = (array) =>
+let strings = ["Ваш баланс"];
+
+const formatGamePoints = (strings, array) =>
+	
 	{
-		let sum = 0;
-		for (let value of array) 
-			{sum += value};
-		console.log (sum);
-	};
+
+	const yourBalance = strings[0];
+
+	let sum = 0;
+	for (let value of array) 
+		{sum += value};
+	console.log(sum);
+
+	let bonus;
+	if (sum > minGamePoints) {
+	bonus = (Math.round(sum * 5 /100)) }
+	else console.log("У вас недостаточно бонусных баллов");
+	
+	let num2str;
+    let n = Math.abs(bonus) % 100; 
+    let n1 = n % 10;
+    let textForms = ["балл", "балла", "баллов"];
+    if (n > 10 && n < 20) { num2str = textForms[2]; };
+    if (n1 > 1 && n1 < 5) { num2str = textForms[1]; };
+    if (n1 == 1) { num2str = textForms[0]; }
+
+    
+    // console.log(yourBalance);
+    // console.log(bonus);
+    // console.log(n);
+    // console.log(n1);
+    // console.log(num2str);
+
+    return `${yourBalance}${bonus} ${num2str}`;
+}
+
+const output = formatGamePoints`Ваш баланс: ${arrayPrices}`;
+console.log(output);
 
 
-const bonus = (array) => 
-	{
-		if (arraySum(array) > minGamePoints) {
-		return arraySum(array) * 5 /100 }
-	};
 
-bonus(arrayPrices);
+let arrayPoints = [74989, 74990, 84990, 62000, 58480, 61800];
 
-console.log(bonus(arrayPrices));
-
-// это для теста второй задачи
-// let a = prompt ('Сколько баллов');
-
-let num2str = function(x) {  
-        let n = Math.abs(x) % 100; 
-        let n1 = n % 10;
-        let textForms = ["балл", "балла", "баллов"];
-        if (n > 10 && n < 20) { return textForms[2]; }
-        if (n1 > 1 && n1 < 5) { return textForms[1]; }
-        if (n1 == 1) { return textForms[0]; }
-        return textForms[2];
-    }
-
-
-console.log(bonus(arrayPrices) + num2str(bonus(arrayPrices)));
 
